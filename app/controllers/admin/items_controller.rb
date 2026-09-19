fe7aad967ea50ad123436ebe70e597590b89fbb5
s5518 <306053809+s5518@>
2026-09-19 07:27:08 +0000

    admin/items/new

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,4 +2,26 @@ class Admin::ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+    @item = Item.new
+    @genres = Genre.all # ジャンルのセレクトボックス用
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item), notice: "商品を登録しました！"
+    else
+      @genres = Genre.all # 失敗した時にもジャンル一覧が必要
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :genre_id, :is_active, :image)
+  end
+
 end
