027f854fc301c51ae1a41ed92aad8f58fb593aaa
trinh <308684744+trinhphuctien0325-afk@users.noreply.github.com>
2026-09-19 07:58:52 +0000

    Add customer registration and withdrawal

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,6 +1,16 @@
 class Public::CustomersController < ApplicationController
-   before_action :authenticate_customer!
+  before_action :authenticate_customer!
+
   def show
-   @customer = current_customer
+    @customer = current_customer
+  end
+
+  def unsubscribe
+  end
+
+  def withdraw
+    current_customer.update(is_active: false)
+    reset_session
+    redirect_to root_path
   end
 end
