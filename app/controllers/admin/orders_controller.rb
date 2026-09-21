31d961c57c8eb7c11a2f4472cead3922dbb6387c
s5518 <306053809+s5518@>
2026-09-21 12:24:20 +0000

    [update]admin/orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -6,6 +6,7 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
+    new_status = params[:order][:status].to_i
     if @order.update(order_params)
       flash[:notice] = "注文ステータスを更新しました。"
       redirect_to admin_order_path(@order)
@@ -17,6 +18,8 @@ class Admin::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:status)
+    params.require(:order).permit(:status).tap do |whitelisted|
+    whitelisted[:status] = whitelisted[:status].to_i if whitelisted[:status].present?
+  end
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,16 @@ class Order < ApplicationRecord
 
   has_many :order_details, dependent: :destroy
 
-enum :payment_method, { credit_card: 0, transfer: 1 }
-enum :status, { waiting_for_payment: 0, payment_confirmation: 1, in_production: 2, preparing_to_ship: 3, shipped: 4 }
+  enum :payment_method, { credit_card: 0, transfer: 1 }
+  enum :status, { waiting_for_payment: 0, payment_confirmation: 1, in_production: 2, preparing_to_ship: 3, shipped: 4 }
+
+  def status_i18n
+    case status
+    when "waiting_for_payment" then "入金待ち"
+    when "payment_confirmation" then "入金確認"
+    when "in_production" then "製作中"
+    when "preparing_to_ship" then "発送準備中"
+    when "shipped" then "発送済み"
+    end
+  end
 end
