03df4122731023400f6ddb2faa94381c611cedc5
s5518 <306053809+s5518@>
2026-09-22 01:11:23 +0000

    add admin/orders

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,22 @@
 class Admin::OrderDetailsController < ApplicationController
+
+  before_action :authenticate_admin!
+
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    if @order_detail.update(order_detail_params)
+      flash[:notice] = "製作ステータスを更新しました。"
+      redirect_to admin_order_path(@order_detail.order)
+    else
+      flash[:alert] = "更新に失敗しました。"
+      redirect_to admin_order_path(@order_detail.order)
+    end
+  end
+
+  private
+
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status)
+  end
+  
 end
