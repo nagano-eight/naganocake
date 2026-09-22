7d1ee732bab584f429f1700e5550e30e1862f430
rinatannn <311713641+rinatannn@users.noreply.github.com>
2026-09-22 22:58:49 +0000

    Add customer information edit functionality

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,6 +5,20 @@ class Public::CustomersController < ApplicationController
     @customer = current_customer
   end
 
+  def edit
+    @customer = current_customer
+  end
+
+  def update
+    @customer = current_customer
+
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   def unsubscribe
   end
 
@@ -12,5 +26,20 @@ class Public::CustomersController < ApplicationController
     current_customer.update(is_active: false)
     reset_session
     redirect_to root_path
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :last_name,
+      :first_name,
+      :last_name_kana,
+      :first_name_kana,
+      :postal_code,
+      :address,
+      :telephone_number,
+      :email
+    )
   end
 end
