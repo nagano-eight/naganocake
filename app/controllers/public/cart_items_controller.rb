389b9306ef8431b01d4444c430d309f52226737b
Itsuki Okamura <ubuntu@ip-172-31-23-48.ap-northeast-1.compute.internal>
2026-09-25 06:41:55 +0000

    cart_itemへ同名の商品の追加修正

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -15,9 +15,15 @@ class Public::CartItemsController < ApplicationController
     if @cart_item.present?
       new_amount = @cart_item.amount + cart_item_params[:amount].to_i
       @cart_item.update(amount: new_amount)
+      redirect_to cart_items_path
     else
-      @cart_item = current_customer.cart_items.new(cart_item_params)
-      @cart_item.save
+      @cart_item = current_customer.cart_items.new(cart_item_params)    
+      if @cart_item.save
+        redirect_to cart_items_path
+      else
+        @item = Item.find(cart_item_params[:item_id])
+        render 'public/items/show'
+      end
     end
     redirect_to cart_items_path
   end
